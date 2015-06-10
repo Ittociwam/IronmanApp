8b7e9d0faf7b192f2d3e8af6e4e22d303c0fb5ea
John Smidt <johnmsmidt@gmail.com>
2015-06-10 13:32:20 -0600

    adding two classes

---
+++ app/src/main/java/cs246/ironmanapp/EntriesGetter.java
@@ -0,0 +1,9 @@
+package cs246.ironmanapp;
+
+/**
+ * Created by John on 6/10/15.
+ */
+public class EntriesGetter extends ServiceCaller {
+
+
+}
---
+++ app/src/main/java/cs246/ironmanapp/ServiceCaller.java
@@ -0,0 +1,11 @@
+package cs246.ironmanapp;
+
+/**
+ * Created by John on 6/10/15.
+ */
+public class ServiceCaller implements Runnable {
+
+    public void run() {
+
+    }
+}
