8fee7f65315e0f0cfe395011dbd28832b6aeed79
Jared <mackiejared66@gmail.com>
2015-07-01 12:42:40 -0600

    java doc
    Signed-off-by: Jared <mackiejared66@gmail.com>

--- app/src/main/java/cs246/ironmanapp/MainActivity.java
+++ app/src/main/java/cs246/ironmanapp/MainActivity.java
@@ -1,31 +1,22 @@
 package cs246.ironmanapp;
 
-
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
-//android:id="@+id/progressBar"
 
 public class MainActivity extends ActionBarActivity {
     private static final String GET_CONTESTANTS_URL = "http://robbise.no-ip.info/ironman/getContestants.php?semester=";
@@ -44,7 +35,6 @@ public class MainActivity extends ActionBarActivity {
     ProgressBar pprogress;
     //EntriesGetter e;
     private final String USER_AGENT = "Mozilla/5.0";
-
     private static final String TAG_MAIN_ACTIVITY = "Main Activity";
 
     @Override
@@ -54,84 +44,12 @@ public class MainActivity extends ActionBarActivity {
         handler = new android.os.Handler();
         getContestants();
         MainActivity.context = MainActivity.this.getApplicationContext();
-
-        ArrayList image_details = getListData();
-        final ListView lv1 = (ListView) findViewById(R.id.custom_list);
-        lv1.setAdapter(new CustomListAdapter(this, image_details));
-        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> a, View v, int POSITION, long id) {
-                Object o = lv1.getItemAtPosition(position);
-                Structs.Contestant listContestants = (Structs.Contestant) o;
-                Toast.makeText(MainActivity.this, "Selected :" + " " + listContestants, Toast.LENGTH_LONG).show();
-            }
-        });
+        //lView = (ListView) findViewById(R.id.rankings);
+        adapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, contestants);
 
 
-        private ArrayList getListData() {
-            ArrayList<Structs.Contestant> results = new ArrayList<Structs.Contestant>();
-            Structs.Contestant arrayContestants = new Structs.Contestant();
-            //newsData.setHeadline("Dance of Democracy");
-            //newsData.setReporterName("Pankaj Gupta");
-            //newsData.setDate("May 26, 2013, 13:35");
-            //results.add(newsData);
-            //lView = (ListView) findViewById(R.id.rankings);
-            //adapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, contestants);
-
-        }
     }
 
-
-    public static class CustomListAdapter extends BaseAdapter {
-        private ArrayList<Structs.Contestant> listData;
-        private LayoutInflater layoutInflater;
-
-        public CustomListAdapter(Context aContext, ArrayList<Structs.Contestant> listData) {
-            this.listData = listData;
-            layoutInflater = LayoutInflater.from(aContext);
-        }
-
-        @Override
-        public int getCount() {
-            return listData.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return listData.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            if (convertView == null) {
-                convertView = layoutInflater.inflate(R.layout.list_row_layout, null);
-                holder = new ViewHolder();
-                holder.u_name = (TextView) convertView.findViewById(R.id.title);
-                holder.percentage = (TextView) convertView.findViewById(R.id.u_name);
-                //holder.reportedDateView = (TextView) convertView.findViewById(R.id.date);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.nameView.setText(listData.get(position).u_name);
-            holder.percentageView.setText(listData.get(position).percentage);
-            //holder.reportedDateView.setText(listData.get(position).getDate());
-            return convertView;
-        }
-
-        static class ViewHolder {
-            TextView nameView;
-            TextView percentageView;
-
-        }
-    }
     public void testProgress(View view) {
 
         gprogress = (ProgressBar) findViewById(R.id.progressBar2);
@@ -145,59 +63,24 @@ public class MainActivity extends ActionBarActivity {
 
     public void getContestants() {
 
-
-    System.out.println("about to do the  run thing");
-        System.out.println("Here in urlDriver()");
-
-        URLReader urlReader = new URLReader();
-
-        //getContestants
-        String semester = "FALL2015";
-        String json = "";
-
-        try {
-            json = urlReader.sendGet("http://robbise.no-ip.info/ironman?semester=" + semester);
-
-        } catch (Exception e) {
-            System.out.println("an error of sorts: ");
-            e.toString();
-            //e.printStackTrace();
-
-        }
-
-        System.out.print("HELLO!!!!!!!!!!!!!!!!!!!!! HERE IS SOME JSON FOR YOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
-
-        System.out.print(json);
-
-        Gson gson = new Gson();
-        Structs.Contestants contestants = gson.fromJson(json, Structs.Contestants.class);
-        
-
-        for (Structs.Contestant contestant : contestants.contestantList) {
-            System.out.println(contestant.u_name + " " + contestant.percentage + "%");
-
-        }
-
-        Log.v(TAG_MAIN_ACTIVITY, "About to do this runnable thing");
-
         new Thread(new Task(GET_CONTESTANTS_URL + getSelectedSemester())).start();
     }
 
 
+    class Task implements Runnable {
+        public String json;
+        private String url;
+        private String params;
 
-
-        class Task implements Runnable {
-            public String json;
-            private String url;
-            private String params;
-            /**
-             * Constructor for a GET request takes only a url ex. "http://robbise.no-ip.info/ironman/getContestants.php?semester=FALL2015"
-             * @param url
-             */
-            public Task(String url) {
-                this.url = url;
-                this.params = "";
-            }
+        /**
+         * Constructor for a GET request takes only a url ex. "http://robbise.no-ip.info/ironman/getContestants.php?semester=FALL2015"
+         *
+         * @param url
+         */
+        public Task(String url) {
+            this.url = url;
+            this.params = "";
+        }
 
         /**
          * CONstructor for a POST request takes a url string and parameters
@@ -236,7 +119,7 @@ public class MainActivity extends ActionBarActivity {
      *
      * @param json
      */
-    public static List populateContestantsList(String json) {
+    public static void populateContestantsList(String json) {
         List<Structs.Contestant> contestList = null;
         try {
             Gson gson = new Gson();
@@ -248,24 +131,17 @@ public class MainActivity extends ActionBarActivity {
                 output += contestant.u_name + " " + contestant.percentage + "%\n";
 
             }
-
-        }catch (Exception e) {
-            Log.e(TAG_MAIN_ACTIVITY, "Error trying to send a GET request with: ", e);
-            e.printStackTrace();
-        }
-        new Thread(runnable).start();
-
-
             Log.v(TAG_MAIN_ACTIVITY, output);
+        } catch (Exception e) {
+            Log.e(TAG_MAIN_ACTIVITY, "Error with gson or outputting or something", e);
+        }
 
-        return contestants;
     }
 
     private String getSelectedSemester() {
         // this method will need to get the semester from a select drop down menu on activity_main.xml
         // and return it.
         return "FALL2015";
-
     }
 
     @Override
@@ -289,6 +165,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
 }
