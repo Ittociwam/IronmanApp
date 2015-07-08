bef67de28e2b22f50431cb17c981a253be0ed65a
Jared <mackiejared66@gmail.com>
2015-07-08 09:12:32 -0600

    update from robbie
    Signed-off-by: Jared <mackiejared66@gmail.com>

--- app/src/main/java/cs246/ironmanapp/MainActivity.java
+++ app/src/main/java/cs246/ironmanapp/MainActivity.java
@@ -1,37 +1,20 @@
 package cs246.ironmanapp;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-<<<<<<< HEAD
-import android.widget.ListAdapter;
-=======
-import android.widget.Button;
 import android.widget.ImageView;
->>>>>>> ef01052e9df24c711eba7810f325e8496f84b2cb
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
