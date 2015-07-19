1510c5c8b350f0d1ddfe3e8450a46b57e6245939
Robbie <ittociwam@hotmail.com>
2015-07-18 22:37:03 -0600

    more comments
    Signed-off-by: Robbie <ittociwam@hotmail.com>

--- app/src/main/java/cs246/ironmanapp/ProgressFinisher.java
+++ app/src/main/java/cs246/ironmanapp/ProgressFinisher.java
@@ -7,13 +7,13 @@ import android.util.Log;
 
 /**
  * Created by Robbie on 7/18/2015.
- * 
+ * This finisher stores the json returned from the progress task
  */
 public class ProgressFinisher implements TaskCompletion {
     private static final String TAG_PROGRESS_FINISHER = "Progress Finisher";
     @Override
     public void finish(Activity activity, String json) {
-        Log.v(TAG_PROGRESS_FINISHER, "The Json in progress finsiher: " + json);
+        Log.v(TAG_PROGRESS_FINISHER, "The Json in progress finisher: " + json);
         try {
             SharedPreferences sharePreferences = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
             SharedPreferences.Editor editor = sharePreferences.edit();
